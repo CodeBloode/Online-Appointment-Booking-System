2736b22f4e2f2591366a7a632547740a173e0f92
petermakss <petermakss42@gmail.com>
2018-06-12 12:00:13 +0300

    I just added the metadata.

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE HTML>
+<HTML lang="en">
+	<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+		<title>home</title>
+
+		<link  rel="stylesheet" href="stylelogin.css" type="text/css" media="all">
+	</head>
+<body>
+	
+	<!--include other php code extension-->
+	
+</body>
+
+</HTML>

947dbe24c85478399390d8423b1bc5fd1d51baa2
Alexogendo <alexnyabuto8@gmail.com>
2018-06-21 09:22:01 +0300

    changes made on signup.css and studentsignup.php

--- studentsignup.php
+++ studentsignup.php
@@ -21,20 +21,32 @@
             <div class="maindiv">
                 <img class="logo" src="images/logo.jpg" alt="logo" height="80px" width="80px" align="center">
                 <div class="head">
-                    <h1>Sign Up </h1>
+                    <h3>Register Here. </h3>
                 </div>
 
+                <!-- This has been arranged according to the way fields are in the database
+                Above all the signup form should capture all the students details in our database
+                -->
                 <div id="container">
                     <p> Registration Number</p>
                     <input type="text" name="regno" maxlength="18" required>
-                     <p> Email </p>
-                      <input type="text" name="email" maxlength="30" required>
 
+                    <p> User Name </p>
+                    <input type="text" name="email" maxlength="40" required>
+
+                    <p> Phone No. </p>
+                    <input type="text" name="email" maxlength="14" required>
+
+                    <p> Email </p>
+                    <input type="text" name="email" maxlength="30" required>
+                    
                     <p>Password </p>
                     <input type="password" name="pass" minlength="6" required><br/>
-
-
-                    <input type="submit" name="signup" value="Sign up" > 
+                    
+                    <p>Confirm Password </p>
+                    <input type="password" name="pass" minlength="6" required><br/>
+                    <br>
+                    <input type="submit" name="signup" value="Register" >
                     <input type="reset" value="Clear"><br/>
                     <p> <i> Having an accout? <a id="loglink" href="studentlogin.php"> <i> login </i></a></p>
                 </div>

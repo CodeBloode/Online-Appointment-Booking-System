1120e947055210a6f8a0da825666802f6a01db6a
Alexogendo <alexnyabuto8@gmail.com>
2018-06-26 00:00:24 -0700

    testing my branch alex-branch

--- studentlogin.php
+++ studentlogin.php
@@ -18,7 +18,7 @@ if (isset($_POST['submit'])) {
 
 ?>
     <html>
-
+<!--Nothing changed yet-->
     <head>
      <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">

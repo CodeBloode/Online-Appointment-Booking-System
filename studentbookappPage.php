6663afded8015255da8864b496fc3454e2a68def
Elvis Mutende <elvismutende@gmail.com>
2018-07-03 20:49:35 -0700

    some forgotten files

--- studentbookappPage.php
+++ studentbookappPage.php
@@ -9,11 +9,18 @@ header('location:studentloginPage.php');
 <!DOCTYPE HTML>
 <html>
   <head>
-    <meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<title>Student Book Appointment</title>
-	<link rel="stylesheet" href="css/stylebook.css" type="text/css">
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Student Book Appointment</title>
+        <link rel="stylesheet" href="css/stylebook.css" type="text/css">
+        <link rel="stylesheet" type="text/css" href="bootstrap/bootstrap.min.css">
+        <link rel="stylesheet" href="css/datepicker.css">
+        <link rel="stylesheet" href="css/timepicker.css">
+        <script type="text/javascript" src="jquery/jquery-3.3.1.js"></script>
+        <script type="text/javascript" src="js/bootstrap-datepicker.js"></script>
+        <script type="text/javascript" src="js/bootstrap.bundle.js"></script>
+        <script type="text/javascript" src="js/timepicker.js"></script>
 
 	<title>Student Book Appointment</title>
 	
@@ -26,11 +33,11 @@ header('location:studentloginPage.php');
     </br>
 
 		<div id="bookcontent">
-		<form action="#" method="post">
+		<form action="students/bookappointment.php" method="post">
              <label>Pick Counsellor</label><br>
                 <select name="counsellor">
                     <option value="null">--NONE--</option>
-                    <option value="counsellor 1"> counsellor 1 </option>
+                    <option value="counselLor 1"> counsellor 1 </option>
                     <option value="counsellor 2"> counsellor 2 </option>
                     <option value="counsellor 3"> counsellor 3 </option>
                     <option value="counsellor 4"> counsellor 4 </option>
@@ -42,17 +49,19 @@ header('location:studentloginPage.php');
 
                 <label>Pick Date </label><br>
                     <input type="text" name="date" id="datepicker" required><br>
+                      <?php include('include/datepicker.php');?>
                 </br>
 
                 <label>Time</label><br>
                 <div id="picktimentime">
 
                     <input type="time" name="settime" id="timepicker" required />
+                    <?php include('include/timepicker.php');?>
                     <br>
 
-                    </div>
+                </div>
                     <br><br>
-                    <input type="submit" name="check" value="Book"/>
+                    <input type="submit" name="book" value="Book"/>
 
 
                     <div id="showsessions">
--- students/bookappointment.php
+++ students/bookappointment.php
@@ -1,115 +1,122 @@
 <?php
-include_once "../include/dbconn.php";
+    include_once "../include/dbconn.php";
+    include_once "studentlogin.php";
 
-class NewAppointment extends DB_con{
-
-private $counsellor;
-private $the_date;
-private $the_time;
-
-    public function __construct()
+    class NewAppointment extends DB_con
     {
-    }
 
-    private function clashingAppointments($tm,$dt,$cnl){
+        public function __construct()
+        {
+        }
+
+        private function clashingAppointments($tm, $dt, $cnl)
+        {
 
 
-        //time to end appointment
-        $endappointment= date('H:i:s',(strtotime($tm)+ 60*45));
+            //time to end appointment
+            $endappointment = date('H:i:s', (strtotime($tm) + 60 * 45));
 
-        $search_if_exist = "select * from sessions where  date= ? AND 
+            $search_if_exist = "select * from all_project_tests.sessions where  date= ? AND 
 						(counsellor = ? AND ((st_time BETWEEN ? AND ?) OR (en_time BETWEEN ? AND ? )))";
 
-        $pre=$this->dbConnection()->prepare($search_if_exist);
-        $pre->execute([$dt,$cnl,$tm,$endappointment,$tm,$endappointment]);
-        $rows=$pre->rowCount();
+            $pre = $this->dbConnection()->prepare($search_if_exist);
+            $pre->execute([$dt, $cnl, $tm, $endappointment, $tm, $endappointment]);
+            $rows = $pre->rowCount();
 
-        if($rows>0){
+            if ($rows > 0) {
 
-            return true;
-        }else{
+                return true;
+            } else {
 
-            return false;
-        }
-
-    }
-
-    private function validateTime($tm){
-
-        //end of working hours in a day
-        $passed_working_time= ($tm= date('H:i:s',(strtotime($tm)+ 60*45)));
-
-        //time to end appointments
-        $stop_time =date('17:30:00');
-
-        //time to start booking appointments;
-        $start_booking = date('08:30:00');
-
-        if(($passed_working_time>$stop_time) or($tm<$start_booking)){
-
-
-            return true;
-        }else{
-
-            return false;
-        }
-
-
-    }
-
-    public function bookAppointment($regno,$names,$couns,$dt,$st_tm,$en_time){
-
-        $errorinBooking = new NewAppointment();
-
-
-
-        if($errorinBooking->clashingAppointments($st_tm,$dt,$couns)==true){
-
-            echo "<script>alert('Time selected for the counsellor is already taken')</script>";
-            //I will add PHP code to get the dates and display the appointments that are booked
-        }else
-
-            if($errorinBooking->validateTime($st_tm)==true){
-
-                echo "<script>alert('select time between 08:30:00 and 16:45:00 and get attended to. Try again')</script>";
-
-        }else{
-
-
-                //create an appointment session
-                $create_Appointment_session = "insert into sessions(studentReg, names ,counsellor, date, st_time, en_time) values
-					('$regno','$couns','$dt','$st_tm','$en_time')";
-
-                try{
-
-                    $new_appointemnt=$this->dbConnection()->exec($create_Appointment_session);
-
-                    header("Location: ../index.php?msg=Appointment Booked Successfully");
-
-                }catch(ErrorException $e){
-
-                    echo "Error ".$e->getMessage();
-                }
-
+                return false;
             }
 
+        }
+
+        private function validateTime($tm)
+        {
+
+            //end of working hours in a day
+            $passed_working_time = ($tm = date('H:i:s', (strtotime($tm) + 60 * 45)));
+
+            //time to end appointments
+            $stop_time = date('17:30:00');
+
+            //time to start booking appointments;
+            $start_booking = date('08:30:00');
+
+            if (($passed_working_time > $stop_time) or ($tm < $start_booking)) {
+
+                return true;
+            } else {
+
+                return false;
+            }
+
+
+        }
+
+        public function bookAppointment($regno, $names, $couns, $dt, $st_tm, $en_time)
+        {
+
+            $errorinBooking = new NewAppointment();
+
+
+            if ($errorinBooking->clashingAppointments($st_tm, $dt, $couns) == true) {
+
+                echo "<script>alert('Time selected for the counsellor is already taken')</script>";
+                //header("Location: ../studentbookappPage.php");
+                echo "<script>window.open('../studentbookappPage.php','_self')</script>";
+                //I will add PHP code to get the dates and display the appointments that are booked
+            } else
+
+                if ($errorinBooking->validateTime($st_tm) == true) {
+
+                    echo "<script>alert('select time between 08:30:00 and 16:45:00 and get attended to. Try again')</script>";
+                   echo "<script>window.open('../studentbookappPage.php','_self')</script>";
+
+                } else {
+
+
+                    //create an appointment session
+                    $create_Appointment_session = "insert into all_project_tests.sessions(studentReg, names ,counsellor, date, st_time, en_time) values
+					('$regno','$names','$couns','$dt','$st_tm','$en_time')";
+
+                    try {
+
+                        $new_appointemnt = $this->dbConnection()->exec($create_Appointment_session);
+
+                        header("Location: ../index.php?msg=Appointment Booked Successfully");
+
+                    } catch (ErrorException $e) {
+
+                        echo "Error " . $e->getMessage();
+                    }
+
+                }
+
+        }
+        public function __debugInfo()
+        {
+            // TODO: Implement __debugInfo() method.
+        }
     }
-}
 
-if(isset($_POST['book'])){
+    if (isset($_POST['book'])) {
 
-    $counsellor_picked = $_POST['counsellor'];
-    $the_date=$_POST['date'];
-    $the_time=$_POST['settime'];
+        $counsellor_picked = $_POST['counsellor'];
+        $the_date = $_POST['date'];
+        $the_time = $_POST['settime'];
 
-    $student_regno =  $_SESSION['regNo'];
-    $student_name= $_SESSION['StudentName'];
+        $student_regno = $_SESSION['regNo'];
+        $student_name = $_SESSION['StudentName'];
 
-    //$_SESSION['email'];
+        //$_SESSION['email'];
 
-    $end_appointment= date('H:i:s',(strtotime($the_time)+ 60*45));
+        $end_appointment = date('H:i:s', (strtotime($the_time) + 60 * 45));
 
-    $bookAppointment = new NewAppointment($student_regno,$student_name,$counsellor_picked,$the_date,$the_time,$end_appointment);
+        $bookAppointment = new NewAppointment();
 
+        $bookAppointment->bookAppointment($student_regno, $student_name, $counsellor_picked, $the_date, $the_time, $end_appointment);
 
-}
+    }

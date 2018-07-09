8dcc4abc096b74507531f2ec2ed5b4a829fbf9f9
petermakss <petermakss42@gmail.com>
2018-07-09 14:44:15 +0300

    naigation bar added

--- include/dbconn.php
+++ include/dbconn.php
@@ -32,13 +32,8 @@ class DB_con{
 		//db details configure according to your system.
 		$this->DB_SERVER='127.0.0.1';
 		$this->DB_USERNAME='root';
-<<<<<<< HEAD
 		$this->DB_PASSWORD='petermakss';
 		$this->DB_DATABASE='all_project_tests';
-=======
-		$this->DB_PASSWORD='hackEd56';
-		$this->DB_DATABASE='appointmentsystem';
->>>>>>> 87d848324b510f66a13e49a110d564056c6f7212
 		$this->CHARSET='utf8mb4';
 
 		 try {

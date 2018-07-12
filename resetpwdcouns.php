5225d7c3df33db444d912f2b584a4cb1ccdc6eee
petermakss <petermakss42@gmail.com>
2018-07-12 11:57:02 +0300

    counsellor reset password GUI

--- resetpwdcouns.php
+++ resetpwdcouns.php
@@ -7,28 +7,24 @@
 </head>
 
 
-
-
-
-
 <body>
 
 
 <div class="title-bar">
-    <div class="center-title">FED360</div>
+    <div class="center-title"> <i>Egerton University</i></div>
 
   </div>
 
 <div class="reset-password-box">
 
   <div class="title-bar">
-    <div class="title">PASSWORD RESET</div>
+    <div class="title"> <i> Forgot Password </i></div>
 
   </div>
 
   <div class="username">
-    <label for="username-input" class="username-label">Username</label>
-    <input type="text" id="username-input" autofocus>
+    <label for="username-input" class="username-label">Registration Number</label>
+    <input type="text" required id="username-input" autofocus >
   </div>
   <!--
   <div class="password">
@@ -36,23 +32,24 @@
     <input type="password" id="password-input">
   </div>
   -->
-
+<!--
   <div class="new-password">
     <label for="new-password-input" class="new-password-label">New Password</label>
     <input type="password" id="new-password-input">
   </div>
 
   <div class="password-verification">
-    <label for="password-input" class="password--verification-label">Password Verification</label>
+    <label for="password-input" class="password-verification-label">Password Verification</label>
     <input type="password" id="password-verification-input">
   </div>
 
-
+    -->
   <div class="back-login">
     <div class="back">
-      <a href="https://codepen.io/OurDailyBread/debug/eJmBNL"><i class="fa fa-angle-double-left"></i> Back to Login</a>
+      <a href="studentloginPage.php"><i class="fa fa-angle-double-left"></i> Back to Login</a>
     </div>
     <div class="reset-password-button" onclick="airTableResetPassword()">
+
       <a href="#">Rest Password</a>
     </div>
   </div>

5c4e9a5c9b3a9fe3b238019fd2ba9a6fca25d7cc
Alexogendo <alexnyabuto8@gmail.com>
2018-07-27 15:44:43 +0300

    a commit

--- studentloginPage.php
+++ studentloginPage.php
@@ -72,7 +72,7 @@ session_start();
                     <img src="images/lock.png" alt="logo" height="40" width="40" style="margin-left: -30px"/> <input type="password" name="password" minlength="6" autocomplete="off" required><br/>
 
                     <input type="submit" name="submit" value="Login" onclick="return(submitlogin());"> <br/>
-                    <a id="forgotpass" href="studentsignupPage.php"> <i> Register new user? </i></a>
+                    <a id="forgotpass" href="studentsignupPage.php"> <i> Create Account? </i></a>
                     <a id="forgotpass" href="students/forgot_pw.php"> <i> Forgot Password? </i></a>
                 </div>
                 </div>

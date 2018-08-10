734cb42a956a514e2bdfcfa312e767ff018f1cb4
justolitunda <litundaz@gmail.com>
2018-08-10 14:23:36 +0300

    chat

---
+++ chat/dbh.php
@@ -0,0 +1,34 @@
+<!DOCTYPE HTML>
+
+<html lang="en">
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<link rel="stylesheet" type="text/css" href="../bootstrap/bootstrapcss/bootstrap.min.css">
+<head>
+    <title>Chat App</title>
+    <link rel="stylesheet" type="text/css" href="../css/coverstyle.css">
+    <script src="../jquery/jquery.min.js"></script>
+
+</head>
+    
+<body>
+<!---chat Text Area-->
+<div class="row">
+    <div class="container">
+        <div class ="col-md-8 col-md-offset-2 display">
+            <centre><h1>WELCOME TO CHAT </h1></center>
+            <div class="message">Counsellors Chat</div>
+            <form method="post">
+                <input type="text" name="userName" class="form-control">
+                <textarea class="form-control" name="message"></textarea>
+                <input type="submit"  value="Send" name="send" class="btn btn-success">
+</div>
+
+
+</body>
+<div style="text-align: center;background-color: SteelBlue; color: white"  class="fixed-bottom">
+        &copy;Copyright 2018. <i>CodeBloode Sons Systems. </i>&checkmark;
+    <br><br>
+    </div>
+</html>
